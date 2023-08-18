c508e612b2a70e4d39f10aca8ec9be0e6e74b611
Alexander Hirdman <126080861+AlexHird@users.noreply.github.com>
2023-08-18 13:10:55 +0200

    feat(667): Post result to jira webhook

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,54 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import axios from "axios";
 
 export async function run() {
   try {
     const octokit = github.getOctokit(core.getInput("github-token"));
+    const jiraUrl = core.getInput("jira-webhook-url");
 
-    // Fetch the latest release
     const { data: latestRelease } = await octokit.rest.repos.getLatestRelease({
       owner: github.context.repo.owner,
-      // owner: 'martinservera',
       repo: github.context.repo.repo,
-      // repo: 'leveransappen-fe'
     });
 
-    if (latestRelease) {
-      console.log(`Latest Release: ${latestRelease.tag_name}`);
-
-      const { data: commits } = await octokit.rest.repos.listCommits({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        sha: latestRelease.target_commitish,
-      });
-
-      const jiraTickets: string[] = [];
-
-      for (const commit of commits) {
-        const match = commit.commit.message.match(/\b(MA-\d+)\b/);
-
-        if (match) {
-          jiraTickets.push(match[1]);
-          console.log(`Found Jira ticket number: ${match[1]}`);
-        }
-      }
-
-      console.log("Jira Issues", jiraTickets);
-    } else {
+    if (!latestRelease) {
       core.warning("No release found.");
     }
+
+    const { data: commits } = await octokit.rest.repos.listCommits({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      sha: latestRelease.target_commitish,
+    });
+
+    const jiraTickets: string[] = [];
+
+    for (const commit of commits) {
+      const match = commit.commit.message.match(/\b(MA-\d+)\b/);
+
+      if (match) {
+        jiraTickets.push(match[1]);
+      }
+    }
+
+    const postBody = JSON.stringify({
+      issues: jiraTickets,
+    });
+
+    const response = await axios.post(jiraUrl, postBody, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status !== 200) {
+      core.setFailed(`Failed to post data to API: ${response.statusText}`);
+    }
+
+    console.log("Jira Issues", jiraTickets);
   } catch (error) {
-    core.setFailed("Something went wrong");
+    core.setFailed(`Action failed with error: ${error}`);
   }
 }
 
